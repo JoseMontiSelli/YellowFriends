467f23bc0d52b18a6d40973732cfe5f95853f0ba
JoseMontiSelli <jose.long@hotmail.com>
2021-06-16 20:42:44 -0300

    Tentativa de conexão com firebase

--- yellowFRIENDS/src/Data/DATA_HistoricoDeConversas.tsx
+++ yellowFRIENDS/src/Data/DATA_HistoricoDeConversas.tsx
@@ -1,72 +1,37 @@
+import React, { useState, useEffect } from "react";
 import { Usuarios, SalasDeChat } from '../Database/Collections'
 
 // Função para pegar o ultimo array
-function lastArrayIndex(someArray: any[]) {
+export function lastArrayIndex(someArray: any[]) {
     var someArrayCopy: any[] = [...someArray]
     let lastIndex: any
     lastIndex = someArrayCopy.pop()
     return lastIndex
 }
-
-// 1. Encontrar e relacionar a ultima mensagem enviada na sala de chat
-const lastMessageTextProvider = (mensagem: any[]) => {
-    var ultimaMensagem: string = lastArrayIndex(mensagem).text
-    return ultimaMensagem
-}
-const lastMessageTimeProvider = (mensagem: any[]) => {
-    var ultimaMensagem: any = lastArrayIndex(mensagem)
-    const data = new Date(ultimaMensagem.DataTime.seconds * 1000)
-    const lastMessageSended = {
-        hora: data.getHours().toString(),
-        minutos: data.getMinutes().toString()
-    }
-    return lastMessageSended
-}
-//2. Encontrar e relacionar se a mensagem foi lida por todos os usuarios da sala de chat
-const isReaded = () => {
-    var readedStatus: boolean
-    return readedStatus = false;
-}
-//3. Trazer da sala de chat a hora da ultima mensagem
-
 //4. Estruturar informações do chat
-function chatRoomDataProvider() {
-    // Lista para armazenar os dados da sala de chat
-    const messageInfos: any = []
-    SalasDeChat.get()
-        .then((snapshot) => {
-            snapshot.forEach((doc) => {
-                // Shortcut para as mensagens das salas de chat
-                var mensagens = doc.data().Mensagens
-                // Informações da sala de chat que eu preciso
-                var data = {
-                    id: doc.data().id,
-                    nome: "",
-                    hora: lastMessageTextProvider(mensagens),
-                    mensagem: lastMessageTimeProvider(mensagens).hora,
-                    avatar: "",
-                    isReaded: true
-                }
-                messageInfos.push(data)
-            })
-        });
-    return messageInfos
+const chatRoomDataProvider = () => {
+    const mensagens: any = []
+    SalasDeChat.get().then((snapshot) => {
+        snapshot.forEach((doc) => {
+            var data = {
+                id: lastArrayIndex(doc.data().Mensagens).IdMensagem,
+                avatar: require('../images/Usuarios/Antonio.jpg'),
+                nome: lastArrayIndex(doc.data().Mensagens).NomeUsuario,
+                mensagem: lastArrayIndex(doc.data().Mensagens).Texto,
+                hora: '20:30',
+                isReaded: true,
+            }
+            mensagens.push(data)
+        })
+    })
+    return mensagens
 }
+
+var testData = chatRoomDataProvider()
+
 const DADOS_CardUsuario = [{
     title: 'Conversas',
-    data: [{
-        id: '1',
-        nome: "",
-        hora: "20",
-        mensagem: "Eu te amo",
-        avatar: require('../images/Usuarios/Antonio.jpg'),
-        isReaded: true
-    }]
+    data: testData
 }];
-
-// {
-//     title: 'Grupos',
-//     data: [chatRoomDataProvider()]
-// }
 
 export default DADOS_CardUsuario;
--- yellowFRIENDS/src/Pages/Home.tsx
+++ yellowFRIENDS/src/Pages/Home.tsx
@@ -7,28 +7,58 @@ import { SectionList } from 'react-native'
 import { Template_CardUsuario } from './Components/Template_CardUsuario'
 import { Template_TittleBubble } from './Components/Template_TitleBubble'
 // Dados pra popular a flatlist
-import DADOS_HistoricoDeConversas from '../Data/DATA_HistoricoDeConversas'
+import { lastArrayIndex } from '../Data/DATA_HistoricoDeConversas'
+import { useEffect } from 'react';
+import { SalasDeChat } from '../Database/Collections';
+import { useState } from 'react';
+import DADOS_ConversasUsuario from '../Data/DATA_HistoricoDeConversas';
 
 export function Home() {
+
+    var [mensagens, setMensagens] = useState([])
+    useEffect(() => {
+        const infosMensagens: any = []
+        SalasDeChat.onSnapshot((snapshot) => {
+            snapshot.forEach((doc) => {
+                var data = {
+                    id: lastArrayIndex(doc.data().Mensagens).IdMensagem,
+                    avatar: require('../images/Usuarios/Antonio.jpg'),
+                    nome: lastArrayIndex(doc.data().Mensagens).NomeUsuario,
+                    mensagem: lastArrayIndex(doc.data().Mensagens).Texto,
+                    hora: '20:30',
+                    isReaded: true,
+                }
+                infosMensagens.push(data)
+            })
+        })
+        setMensagens(infosMensagens)
+    }, [])
+
+    const DADOS_CardUsuario = [{
+        title: 'Conversas',
+        data: mensagens
+    }];
+
+    console.log(DADOS_CardUsuario[0].data)
     return (
         <SafeAreaView style={home.main}>
             {/* LISTA DE CARDS DO USUARIO */}
             <SectionList style={home.mainCards}
-            // De onde vem os dados do usuario
-                sections={DADOS_HistoricoDeConversas} 
+                // De onde vem os dados do usuario
+                sections={DADOS_CardUsuario}
                 // key que define que o item é unico
-                keyExtractor={(item, index) => item.id} 
-                renderSectionHeader = {({section: {title}}) => <Template_TittleBubble
-                text = {title}
+                keyExtractor={(item, index) => item.id}
+                renderSectionHeader={({ section: { title } }) => <Template_TittleBubble
+                    text={title}
                 />}
 
-                renderItem = {({ item }) => 
-                <Template_CardUsuario  // Template da lista
-                    nome={item.nome}
-                    hora={item.hora}
-                    mensagem={item.mensagem}
-                    avatar={item.avatar}
-                    isReaded={item.isReaded} />
+                renderItem={({ item }) =>
+                    <Template_CardUsuario  // Template da lista
+                        nome={item.nome}
+                        hora={item.hora}
+                        mensagem={item.mensagem}
+                        avatar={item.avatar}
+                        isReaded={item.isReaded} />
                 } />
         </SafeAreaView>
     )
